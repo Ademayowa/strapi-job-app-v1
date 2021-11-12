f6c1108892f2f7f7085b4caf9a475f55dd07384b
Ademayowa <ademayowaadeniyi@gmail.com>
2021-11-12 20:43:59 +0100

    added document.js

--- pages/jobs/[slug].js
+++ pages/jobs/[slug].js
@@ -1,25 +1,32 @@
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Col } from 'react-bootstrap';
 import { API_URL } from '@/config/index';
 import Layout from '@/components/Layout';
 import BreadCrumb from './breadCrumb';
+import JobInfo from './jobInfo';
 
 export default function SingleJobPage({ jb }) {
-  const { role } = jb;
+  const { role, type, description } = jb;
 
   return (
     <Layout title={jb.role}>
-      <div className='company'>
-        {/* Breadcrumb */}
-        <BreadCrumb />
-        {/* Job info => in a flex form */}
-        {/* Job title */}
-        {/* Job type */}
-        {/* ====== */}
-        {/* Overview  */}
-        {/* qualifications reqiured  */}
-        {/* company */}
-        <h3>{role}</h3>
-      </div>
+      <BreadCrumb />
+      <Container>
+        <JobInfo />
+        <h3 className='fs-1 fw-bold mt-5 mb-3'>{role}</h3>
+        <h4>{type}</h4>
+        <h5 className='mt-5 fw-bold'>Overview</h5>
+        <Col lg={7}>
+          <p>{description}</p>
+        </Col>
+
+        <h5 className='mt-5 fw-bold'>Key Qualifications</h5>
+      </Container>
+      {/* Job title */}
+      {/* Job type */}
+      {/* ====== */}
+      {/* Overview  */}
+      {/* qualifications reqiured  */}
+      {/* company */}
     </Layout>
   );
 }
---
+++ pages/jobs/jobInfo.js
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+
+export default function JobInfoPage() {
+  return (
+    <div className='d-flex justify-content-between mt-4 mb-4 fw-bold'>
+      <Link href='#'>
+        <a>overview</a>
+      </Link>
+
+      <Link href='#'>
+        <a>key qualifications</a>
+      </Link>
+
+      <Link href='#'>
+        <a>perks</a>
+      </Link>
+
+      <Link href='#'>
+        <a>company</a>
+      </Link>
+    </div>
+  );
+}
