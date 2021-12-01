e3d94e3a2f418f3c625b37b0dafeafb07334daa5
Ademayowa <ademayowaadeniyi@gmail.com>
2021-12-01 13:27:42 +0100

    built login and register page

--- pages/account/login.js
+++ pages/account/login.js
@@ -44,7 +44,7 @@ export default function LoginPage() {
                   <Form.Label>Password</Form.Label>
                   <Form.Control
                     type='password'
-                    placeholder='Password'
+                    placeholder='Enter Password'
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                     required
