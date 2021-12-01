e91f7fb99798fa46e4d34aea7b9b80f58261aa39
Ademayowa <ademayowaadeniyi@gmail.com>
2021-12-01 18:01:53 +0100

    refactored login & register page

--- components/FormContainer.js
+++ components/FormContainer.js
@@ -2,7 +2,7 @@ import { Container, Row, Col } from 'react-bootstrap';
 
 export default function FormContainer({ children }) {
   return (
-    <Container className='mt-5'>
+    <Container className='mt-5 mb-5'>
       <Row>
         <Col lg={5} className='mx-auto'>
           {children}
